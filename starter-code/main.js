c51b629715009e17a92bd3960b5f6e863361455f
merryschurr <merryschurr@gmail.com>
2016-10-30 22:02:03 -0600

    created alert message

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,30 @@
 console.log("JS file is connected to HTML! Woo!")
+
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+if (cardOne=='queen' && cardTwo=='queen'){
+alert('You found a match!');
+}else {
+alert('Sorry, try again.');
+}
+
+if (cardThree=='king' && cardFour=='king'){
+alert('You found a match!');
+}else {
+alert('Sorry, try again.');
+}
+
+if (cardOne=='queen' && cardThree=='king'){
+alert('Sorry, try again.');
+}else {
+alert('You found a match!');
+}
+
+if (cardTwo=='queen' && cardFour=='king'){
+alert('Sorry, try again.');
+}else {
+alert('You found a match!');
+}
